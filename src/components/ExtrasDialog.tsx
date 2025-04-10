b52b686342b64079ce3331ebf27adfc2cdd798e7
John <johnlouisstephenson@gmail.com>
2025-04-10 13:58:58 +0100

    add: max selected message

--- src/components/ExtrasDialog.tsx
+++ src/components/ExtrasDialog.tsx
@@ -1,4 +1,4 @@
-import { DisplayItem, Extra } from "@/utils/types/Menu";
+import { useForm } from "react-hook-form";
 import {
   Dialog,
   DialogContent,
@@ -7,10 +7,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
-import { formatPrice } from "@/utils/utils";
 import { Button } from "./ui/button";
 import toast from "react-hot-toast";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { formatPrice } from "@/utils/utils";
+import { DisplayItem, Extra } from "@/utils/types/Menu";
 
 type ExtrasDialogProps = {
   extras: Extra[];
@@ -24,11 +25,28 @@ export function ExtrasDialog({
   maxSelectCount,
 }: ExtrasDialogProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const { register, reset, watch } = useForm();
+
   function notify(event) {
     event.preventDefault();
     toast.success(`${displayItem.Name} added to basket`);
     setIsOpen(false);
   }
+
+  useMemo(() => {
+    if (!isOpen) reset();
+  }, [isOpen, reset]);
+
+  const formData = watch();
+
+  const selectedExtras = extras.filter((extra) =>
+    formData.extras ? formData.extras[extra.Id] === true : false,
+  );
+
+  console.log(formData);
+  console.log(selectedExtras);
+
+  const isMaximum = selectedExtras.length === maxSelectCount;
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -54,13 +72,22 @@ export function ExtrasDialog({
                     <label htmlFor={extra.Name}>
                       {`${extra.Name} (+${formatPrice(extra.Price)})`}
                     </label>
-                    <input id={extra.Name} type="checkbox" />
+                    <input
+                      {...register(`extras.${extra.Id}`)}
+                      id={extra.Name}
+                      type="checkbox"
+                    />
                   </div>
                 ))
               ) : (
                 <p>
                   Add {displayItem.Name} to cart for{" "}
                   {formatPrice(displayItem.Price)}
+                </p>
+              )}
+              {isMaximum && (
+                <p className="text-red-700">
+                  Maximum number of extras selected
                 </p>
               )}
             </div>
