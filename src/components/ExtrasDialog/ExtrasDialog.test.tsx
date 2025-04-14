0544fe41329475749112d151bd3f63a6ae6cf7e4
John <johnlouisstephenson@gmail.com>
2025-04-14 15:01:42 +0100

    add: passing buttons length test for dialog

--- src/components/ExtrasDialog/ExtrasDialog.test.tsx
+++ src/components/ExtrasDialog/ExtrasDialog.test.tsx
@@ -1,15 +1,27 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
-import menuData from "@/__mocks__/menuData.json";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import menuData from "@/__mocks__/menuData.json";
 import { Menu } from "../Menu/Menu";
+import { transformMenu } from "@/utils/transformMenu/transformMenu";
 
 const server = setupServer(
   http.get("https://menus.flipdish.co/prod/16798/*", () => {
     return HttpResponse.json(menuData);
   }),
 );
+
+const mockMenu = transformMenu(menuData);
+const mockDisplayItems = mockMenu.MenuSections.map(
+  (section) => section.MenuItems,
+)
+  .map((item) => item.map((item) => item.DisplayItems))
+  .flat(2);
+
+const mockExtras = mockMenu.MenuSections.map((section) => section.MenuItems)
+  .map((item) => item.map((item) => item.Extras))
+  .flat(2);
 
 describe("extras dialog", () => {
   let queryClient: QueryClient;
@@ -34,10 +46,8 @@ describe("extras dialog", () => {
     });
 
     it("renders add buttons", async () => {
-     
-        const addButtons = await screen.findAllByText("+");
-        expect(addButtons.length).toBeGreaterThan(0);
-     
+      const addButtons = await screen.findAllByText("+");
+      expect(addButtons).toHaveLength(mockDisplayItems.length);
     });
 
     it("opens dialog when clicked", async () => {
